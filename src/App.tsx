39618d9e53e9a44566b72141ba82cf8d25171275
Birgit <birgit@hey.com>
2022-07-26 20:15:35 +0200

    adding TextField

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,16 @@
-
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Start from "./pages/Start";
+import Agenda from "./pages/Agenda";
 
 const App = () => {
   return (
-    <div>
-      test   
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Start />} />
+        <Route path="/agenda" element={<Agenda />} />
+      </Routes>
+    </BrowserRouter>
   )
 }
 
